0a0350cabc843c8cc68c571be7ed313bda435fde
ikarashi <4655ryo1298@gmail.com>
2019-07-08 09:12:31 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 五十嵐諒太 on 2019/07/08.
+//  Copyright © 2019 ikarashiryota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
